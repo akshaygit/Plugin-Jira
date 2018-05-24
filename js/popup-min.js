bf44c5120e036822299abb8ccca65b1b78ae1ef6
akshay <bisht.akshay007@gmail.com>
2018-05-24 14:20:44 +0000

    Initial comit

---
+++ js/popup-min.js
@@ -0,0 +1,1 @@
+var modal=document.getElementById("myModal");var btn=document.getElementById("myBtn");var span=document.getElementsByClassName("close")[0];btn.onclick=function(){modal.style.display="block"};span.onclick=function(){modal.style.display="none"};window.onclick=function(a){if(a.target==modal){modal.style.display="none"}};
